f7e13ee494842f01c5c8eb6b49d527e84bd1c986
Gerson Tiongson <gerson.tiongson.12314@gmail.com>
2026-04-29 20:12:24 +0800

    checkout features

--- frontend/src/app/core/components/checkout/checkout.component.ts
+++ frontend/src/app/core/components/checkout/checkout.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormService } from '../../services/form.service';
+import { Observable, take } from 'rxjs';
+import { State } from '../../../common/state';
+import { StackShopValidators } from '../../validators/stack-shop-validators';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -11,48 +15,55 @@ import { FormService } from '../../services/form.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
   fb = inject(FormBuilder);
   formService = inject(FormService);
+  cartService = inject(CartService);
   checkoutForm: FormGroup;
   totalQuantity: number = 0;
   totalPrice: number = 0;
+  isSameAsShipping = signal<boolean>(true);
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+  countries$ = this.formService.getCountries();
+  shippingAddressStates$!: Observable<State[]>;
+  billingAddressStates$!: Observable<State[]>;
 
   constructor() {
     this.checkoutForm = this.fb.group({
       customer: this.fb.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        lastName: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        email: ['', [Validators.required, Validators.email]]
       }),
       shippingAddress: this.fb.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: ['']
+        country: ['', [Validators.required]],
+        street: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        city: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        state: ['', [Validators.required]],
+        zipCode: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]]
       }),
       billingAddress: this.fb.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: ['']
+        country: ['', [Validators.required]],
+        street: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        city: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        state: ['', [Validators.required]],
+        zipCode: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]]
       }),
       creditCard: this.fb.group({
-        cartType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
-        expirationMonth: [''],
-        expirationYear: [''],
+        cartType: ['', [Validators.required]],
+        nameOnCard: ['', [Validators.required, Validators.minLength(2), StackShopValidators.notOnlyWhitespace]],
+        cardNumber: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16), StackShopValidators.notOnlyWhitespace, Validators.pattern('^[0-9]+$')]],
+        securityCode: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(4), StackShopValidators.notOnlyWhitespace, Validators.pattern('^[0-9]+$')]],
+        expirationMonth: ['', [Validators.required]],
+        expirationYear: ['', [Validators.required]],
       }),
     });
 
-    this.populateCreditCardMonthsAndYears();
+  }
 
+  ngOnInit(): void {
+    this.getCreditCardMonthsAndYears();
   }
 
   submitOrder(form: FormGroup) {
@@ -60,21 +71,28 @@ export class CheckoutComponent {
       console.log('Order submitted:', form.value);
       // Here you would typically send the order data to your backend API
     } else {
-      console.log('Form is invalid');
+      this.checkoutForm.markAllAsTouched();
     }
+
   }
-  sameAsShipping(e: any) {
+  toggleBillingAddressForm(e: any) {
     const billingAddress = this.checkoutForm.get('billingAddress');
     const shippingAddress = this.checkoutForm.get('shippingAddress');
 
     if (e.target.checked) {
+      // set the billing form value similar to shipping form value
       billingAddress?.setValue(shippingAddress?.value);
     } else {
-      this.checkoutForm.controls['billingAddress'].reset();
+      // reset the billing form but keep the country and state values to avoid unnecessary API calls to fetch states when the user checks the checkbox again
+      billingAddress?.reset({
+        country: billingAddress?.get('country')?.value,
+        state: billingAddress?.get('state')?.value
+      });
     }
+    this.isSameAsShipping.set(!this.isSameAsShipping());
   }
 
-  populateCreditCardMonthsAndYears() {
+  getCreditCardMonthsAndYears() {
     const startMonth = new Date().getMonth() + 1;
     this.creditCardMonths = this.formService.getCrediCardMonths(startMonth);
     this.creditCardYears = this.formService.getCreditCardYears();
@@ -89,7 +107,108 @@ export class CheckoutComponent {
     } else {
       startMonth = 1;
     }
-    this.formService.getCrediCardMonths(startMonth);
+    this.creditCardMonths = this.formService.getCrediCardMonths(startMonth);
   }
 
+  onCountryChange(event: Event, target: 'shippingAddress' | 'billingAddress') {
+    const country = (event.target as HTMLSelectElement).value;
+    const states$ = this.formService.getStates(country);
+
+    if (target === 'shippingAddress') {
+      this.shippingAddressStates$ = states$;
+    } else {
+      this.billingAddressStates$ = states$;
+    }
+
+    // set the state input value to the first item of the states list, use take(1) to complete the observable after the first emission
+    states$
+      .pipe(take(1))
+      .subscribe(states => {
+        const stateControl = this.checkoutForm.get(`${target}.state`);
+        if (stateControl) {
+          stateControl.setValue(states[0]?.name || '');
+        }
+      });
+  }
+
+  get firstName() {
+    return this.checkoutForm.get('customer.firstName');
+  }
+
+  get lastName() {
+    return this.checkoutForm.get('customer.lastName');
+  }
+
+  get email() {
+    return this.checkoutForm.get('customer.email');
+  }
+
+
+  // SHIPPING FORM GETTER
+  get shippingAddressCity() {
+    return this.checkoutForm.get('shippingAddress.city');
+  }
+
+  get shippingAddressStreet() {
+    return this.checkoutForm.get('shippingAddress.street');
+  }
+
+  get shippingAddressState() {
+    return this.checkoutForm.get('shippingAddress.state');
+  }
+
+  get shippingAddressZipCode() {
+    return this.checkoutForm.get('shippingAddress.zipCode');
+  }
+
+  get shippingAddressCountry() {
+    return this.checkoutForm.get('shippingAddress.country');
+  }
+
+
+  // BILLING FORM GETTER
+  get billingAddressCity() {
+    return this.checkoutForm.get('billingAddress.city');
+  }
+
+  get billingAddressStreet() {
+    return this.checkoutForm.get('billingAddress.street');
+  }
+
+  get billingAddressState() {
+    return this.checkoutForm.get('billingAddress.state');
+  }
+
+  get billingAddressZipCode() {
+    return this.checkoutForm.get('billingAddress.zipCode');
+  }
+
+  get billingAddressCountry() {
+    return this.checkoutForm.get('billingAddress.country');
+  }
+
+  // CREDIT CARD FORM GETTER
+  get creditCardCartType() {
+    return this.checkoutForm.get('creditCard.cartType');
+  }
+
+  get creditCardNameOnCard() {
+    return this.checkoutForm.get('creditCard.nameOnCard');
+  }
+
+  get creditCardNumber() {
+    return this.checkoutForm.get('creditCard.cardNumber');
+  }
+
+  get creditCardSecurityCode() {
+    return this.checkoutForm.get('creditCard.securityCode');
+  }
+
+  get creditCardExpirationMonth() {
+    return this.checkoutForm.get('creditCard.expirationMonth');
+  }
+
+  get creditCardExpirationYear() {
+    return this.checkoutForm.get('creditCard.expirationYear');
+  }
 }
